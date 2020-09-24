3d48c471871aa4e4756edac76c6a11394688f8d8
wymerkd <wymerkd@gmail.com>
2020-09-24 09:06:57 -0700

    add json data for two sample beers to test props

--- src/beerData.js
+++ src/beerData.js
@@ -1,10 +1,18 @@
 const beerData = [
   {
-    
+    'beerName': 'Beer One',
+    'beerDetails': 'Lorem ipsum, dolor sit amet consectetur adipiscing elit',
+    'alcoholContent': '5.5% ABV',
+    'firstPrice': '$6',
+    'secondPrice': '$9'
   },
-  {
-
-  }
+   {
+     'beerName': 'Beer Two',
+     'beerDetails': 'Lorem amet consectetur adipiscing elit ipsum, dolor sit ',
+     'alcoholContent': '4.5% ABV',
+     'firstPrice': '$5',
+     'secondPrice': '$8'
+   }
 ]
 
 export default beerData;
