ab697c7e50fc3ccac8d6667c60e715fd7192955f
wymerkd <wymerkd@gmail.com>
2020-09-24 09:23:13 -0700

    set state and props in App.js to json object, change App function to class, render component and pass state to home.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 // React Router
 import { Switch, Route } from 'react-router-dom'; 
 
+// Test json data object
+import beerData from './beerData.js';
+
 // CSS
 import './App.scss';
 
@@ -14,15 +17,25 @@ import Home from './components/Home';
 import AdminView from './components/AdminView';
 // import Error404 from './components/Error404';
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path ='/' component={Home} />
-        <Route path ='/adminview' component={AdminView} />
-      </Switch>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        masterBeerList: beerData,
+        tapList: beerData[0]
+      };
+    }
+
+    render(){
+      return (
+        <div>
+          <Switch>
+            <Route exact path='/' render={()=><Home tapList={this.state.tapList}/>} />
+            <Route path='/adminview' component={AdminView} />
+          </Switch>
+        </div>
+      );
+    }
+  }
 
 export default App;
